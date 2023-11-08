cdb5554819b4344d0f9f91c3fe3397a07744c347
clementino lima <clementinodev@gmail.com>
2023-11-08 15:49:57 -0300

    primeira modifição

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-dsnjdnjdnfn
+let experiencia = 7.000
+let nickname = "Mestre Do Code"
+
+if (experiencia === 1.000){
+  console.log("Ferro")
+}else if(experiencia === 2.000){
+  console.log("Bronze")
+}else if(experiencia === 5.000){
+  console.log("Prata")
+}else if(experiencia === 7.000){
+  console.log("Ouro")
+}else if(experiencia === 8.000){
+  console.log("Platina")
+}else if(experiencia === 9.000){
+  console.log("Ascendente")
+}else if(experiencia === 10.000){
+  console.log("Imortal")
+}else if(experiencia === 10.001){
+  console.log("Radiante")
+}
+console.log ("Heroi de Nome : "  + nickname)
