659afd8c3358fe67e2c4bded6c30ccfa69996dc1
clementino lima <clementinodev@gmail.com>
2023-11-11 14:25:01 -0300

    projeto atualizado

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-let experiencia = 7.000
-let nickname = "Mestre Do Code"
+let nomeDoHeroi = "Mestre Do Code";
+let experiencia = 7031
 
-if (experiencia === 1.000){
-  console.log("Ferro")
-}else if(experiencia === 2.000){
-  console.log("Bronze")
-}else if(experiencia === 5.000){
-  console.log("Prata")
-}else if(experiencia === 7.000){
-  console.log("Ouro")
-}else if(experiencia === 8.000){
-  console.log("Platina")
-}else if(experiencia === 9.000){
-  console.log("Ascendente")
-}else if(experiencia === 10.000){
-  console.log("Imortal")
-}else if(experiencia === 10.001){
-  console.log("Radiante")
+let nivel
+if (experiencia < 1000) {
+    nivel = "Ferro"
+} else if (experiencia >= 1001 && experiencia <= 2000) {
+    nivel = "Bronze"
+} else if (experiencia >= 2001 && experiencia <= 5000) {
+    nivel = "Prata"
+} else if (experiencia >= 6001 && experiencia <= 7000) {
+    nivel = "Ouro"
+} else if (experiencia >= 7001 && experiencia <= 8000) {
+    nivel = "Platina"
+} else if (experiencia >= 8001 && experiencia <= 9000) {
+    nivel = "Ascendente"
+} else if (experiencia >= 9001 && experiencia <= 10000) {
+    nivel = "Imortal"
+} else if (experiencia >= 10001) {
+    nivel = "Radiante"
 }
-console.log ("Heroi de Nome : "  + nickname)
+
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivel}`)
